a3e95a5646fb751ce1614f7158cf150eb3841708
amiv.80 <amir.r.v138@gmail.com>
2023-06-21 22:00:50 +0330

    add tutorial, overview html file and fix and improve styles, formatting.

--- core/views.py
+++ core/views.py
@@ -1,41 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def game_view(request):
     if request.method == 'POST':
-        goal = get_goals(request.POST)
-        total_scores = (get_largest_blocks(request.POST) + get_goal_scores(request.POST)) - get_roads(request.POST)
+        try:
+            goal = get_goals(request.POST)
+            total_scores = calculate_total_scores(request.POST)
+            result = 'You Win.' if total_scores >= goal else 'You Lose.'
+            context = {'goal': goal, 'total_scores': total_scores, 'result': result}
+            return render(request, 'game/home.html', context)
+        except Exception as e:
+            error_message = f"An error occurred: {str(e)}"
+            return HttpResponse(error_message, status=500)
+    return render(request, 'game/home.html')
 
-        if total_scores >= goal:
-            result = 'You Win.'
-        else:
-            result = 'You Lose.'
 
-        return render(request, 'game/game.html', {'goal': goal, 'total_scores': total_scores, 'result': result})
+def tutorial_view(request):
+    return render(request, 'game/tutorial.html')
 
-    return render(request, 'game/game.html')
+
+def overview_view(request):
+    return render(request, 'game/overview.html')
+
 
 def get_goals(data):
-    goals = []
-    for i in range(3):
-        goal = int(data.get(f"goal_{i+1}"))
-        goals.append(goal)
+    goals = [int(data.get(f"goal_{i+1}")) for i in range(3)]
     return sum(goals)
 
-def get_largest_blocks(data):
+
+def calculate_total_scores(data):
     block_types = ['Park', 'Residential', 'Commercial', 'Industrial']
-    blocks = []
-    for block_type in block_types:
-        block = int(data.get(f"block_{block_type.lower()}"))
-        blocks.append(block)
-    return sum(blocks)
-
-def get_goal_scores(data):
-    goal_scores = []
-    for i in range(3):
-        goal_score = int(data.get(f"goal_score_{i+1}"))
-        goal_scores.append(goal_score)
-    return sum(goal_scores)
-
-def get_roads(data):
+    blocks = [int(data.get(f"block_{block_type.lower()}")) for block_type in block_types]
+    goal_scores = [int(data.get(f"goal_score_{i+1}")) for i in range(3)]
     roads = int(data.get('roads'))
-    return roads
+    return sum(blocks) + sum(goal_scores) - roads
